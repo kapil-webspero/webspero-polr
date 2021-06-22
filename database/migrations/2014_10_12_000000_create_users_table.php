947066b038d5d68b806f828066001f1368392a08
Kapil <kapil@webspero.com>
2021-06-22 18:12:52 +0530

    DB integration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->text('ip');
+            $table->string('recovery_key');
+            $table->string('role');
+            $table->string('active');
+
+            $table->string('api_key')->nullable();
+            $table->boolean('api_active')->default(0);
+            $table->string('api_quota')->default(60);
+
             $table->rememberToken();
             $table->timestamps();
+
+            $table->softDeletes();
+
         });
     }
 
---
+++ database/migrations/2021_06_22_123230_create_links_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('short_url');
+            $table->longText('long_url');
+            $table->string('ip');
+            $table->string('creator');
+            $table->string('clicks')->default(0);
+            $table->string('secret_key');
+
+            $table->boolean('is_disabled')->default(0);
+            $table->boolean('is_custom')->default(0);
+            $table->boolean('is_api')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
